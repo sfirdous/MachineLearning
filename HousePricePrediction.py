2631c6256a2a166d90b2a90241ed23e2242497d2
firdous <sfirdous0404@gmail.com>
2024-08-01 21:08:31 +0530

    - Selected relevant features and target variable - Split data into training and test sets - Scaled features using StandardScaler - Trained Linear Regression model - Predicted and evaluated model performance

--- HousePricePrediction.py
+++ HousePricePrediction.py
@@ -23,6 +23,16 @@ print(boston_df.info())
 #checking for missing values
 print(boston_df.isnull().sum())
 
+print(np.isinf(boston_df).sum())
+
+boston_df.fillna(boston_df.median(), inplace=True)
+
+# Replace infinity with NaN
+boston_df.replace([np.inf, -np.inf], np.nan, inplace=True)
+
+# Fill those NaNs (formerly infinities) with the median value of each column
+boston_df.fillna(boston_df.median(), inplace=True)
+
 #step3.Exporatory Data Analysis
 #summary
 print(boston_df.describe())
@@ -38,5 +48,34 @@ plt.show()
 #calculaing the correlation
 correlation_matrix = boston_df.corr()
 sns.heatmap(correlation_matrix,annot = True,cmap='coolwarm',fmt='.2f')
+# plt.show()
+
+# Select features and target variable
+features = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT']
+X = boston_df[features]
+y = boston_df['PRICE']
+
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=42)
+
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.fit_transform(X_test)
+
+model = LinearRegression()
+model.fit(X_train,y_train)
+
+y_pred = model.predict(X_test)
+
+mse = mean_squared_error(y_test,y_pred)
+print(f'Mean Sqaure error : {mse}')
+r2_s = r2_score(y_test,y_pred)
+print(f'R2 Score {r2_s}')
+
+plt.figure(figsize=(14,10))
+plt.scatter(y_test,y_pred,edgecolor='k',alpha=0.7)
+plt.plot([y_test.min(),y_test.max()],[y_test.min(),y_test.max()],'r--',lw = 3)
+plt.xlabel('Actual')
+plt.ylabel('Predicted')
+plt.title('Actual vs Predicted House Prices')
 plt.show()
 
