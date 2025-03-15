0329ad1576b2e8af56b6a0a31be955f2530416fa
Shaikh Firdous <126231373+sfirdous@users.noreply.github.com>
2025-03-15 20:13:11 +0530

    Delete SC562-ML I directory

--- SC562-ML I/StatisticalLearning (ISLR Chapter 2)/Q9.R
+++
@@ -1,9 +0,0 @@
-auto = read.csv("ML-LabWork/datasets/Auto.csv") 
-View(auto)
-
-# Response Variable - mpg
-# Quantitative predictors - displacement,horsepower,weight,acceleration
-# Qualitative  predictors - cylinders,year,origin,name
-
-range = NULL
-quantitative_p = c(3,4,5,6)
